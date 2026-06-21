3dec8debc537389dba8802d33b6a1ab2902381d3
wen43008-svg <wen43008@gmail.com>
2026-06-21 14:18:44 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kakeibo-v10';
+const CACHE='kakeibo-v11';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
 self.addEventListener('fetch',e=>{
